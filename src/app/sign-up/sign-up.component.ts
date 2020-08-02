79ba2a19f41d53704686e0e41487dae0664f0971
devandscorp <artemda459@gmail.com>
2020-08-02 22:58:30 +0300

    feat: add sign up page

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  form: FormGroup;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      firstName: new FormControl(null, [Validators.required]),
+      lastName: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    })
+  }
+  submit() {
+    console.log(this.form.value);
+  }
+}
